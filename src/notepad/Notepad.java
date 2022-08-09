c14154b3d0a16787c3f32e5073ff64b84487ae84
anuj123-45 <anuj1358.be20@chitkara.edu.in>
2022-08-09 10:58:54 +0530

    changes done

--- src/notepad/Notepad.java
+++ src/notepad/Notepad.java
@@ -11,39 +11,29 @@ import java.awt.image.ImageProducer;
 import java.lang.reflect.Array;
 import java.util.Locale;
 import javax.swing.text.StyleConstants;
-import jdk.nashorn.internal.objects.NativeDebug;
-import sun.security.x509.OIDMap;
+import java.awt.event.*;
 
 /**
  *
  * @author Anuj
  */
-
-
-
-
-
-public class Notepad {
-   
-
-    public static  void main(String[] args) {
-
-        
+public class Notepad implements ActionListener{
+     JTextArea t;
+    public Notepad() {
         // Main frame
-       JFrame f= new JFrame("Notepad");
-       f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-       
-       // title bar image
-        Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Anuj\\Documents\\NetBeansProjects\\Notepad\\src\\notepad\\image.jpg");    
-         f.setIconImage(icon); 
+        JFrame f = new JFrame("Notepad");
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // title bar image
+        Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Anuj\\Documents\\NetBeansProjects\\Notepad\\src\\notepad\\image.jpg");
+        f.setIconImage(icon);
         f.setBounds(100, 100, 800, 600);
-       
+
         // for text area
-        
-        JTextArea t=new JTextArea();
-        t.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,18));
-   
-           // for menu bar
+        t=new JTextArea();
+        t.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+
+        // for menu bar
         JMenuBar mb = new JMenuBar();
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
@@ -51,6 +41,7 @@ public class Notepad {
 
         // for file menu
         JMenuItem newfile = new JMenuItem("New");
+
         JMenuItem openfile = new JMenuItem("Open");
         JMenuItem savefile = new JMenuItem("Save");
         JMenuItem printfile = new JMenuItem("Print");
@@ -82,53 +73,50 @@ public class Notepad {
         help.add(about);
 
         // adding menus in menubar
-       mb.add(file);
-       mb.add(edit);
-       mb.add(help);
+        mb.add(file);
+        mb.add(edit);
+        mb.add(help);
 
-       // when click on about (menuitem)
+//        when click on about (menuitem)
         about.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
-                JFrame frameAction = new JFrame();
+                try {
+                    JFrame frameAction = new JFrame();
 
-                 frameAction.setSize(500, 400);
-                JLabel ln = new JLabel("<HTML>We"
-                        + "welcome to Notepad<br>Notepad is a simple text editor for Microsoft Windows and a basic text-editing "
-                        + " program which enables computer users to create document<br>All rights are reserved@2022</html>");
-                  
-        Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Anuj\\Documents\\NetBeansProjects\\Notepad\\src\\notepad\\image.jpg");    
-        frameAction.setIconImage(icon); 
-                
-                ln.setBounds(100,-50,300,300);
-                
-                frameAction.setTitle("About Section");
-                frameAction.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                frameAction.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,12));
-               
-                frameAction.setLocationRelativeTo(null);
+                    frameAction.setSize(500, 400);
+                    JLabel ln = new JLabel("<HTML>We"
+                            + "welcome to Notepad<br>Notepad is a simple text editor for Microsoft Windows and a basic text-editing "
+                            + " program which enables computer users to create document<br>All rights are reserved@2022</html>");
 
-                       frameAction.add(ln);
-                       frameAction.setLayout(null);
-            frameAction.setVisible(true);
-            
-                about.add(frameAction);
-                }
-         
-                catch(Exception exp){
-                System.out.println(exp);
+                    Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Anuj\\Documents\\NetBeansProjects\\Notepad\\src\\notepad\\image.jpg");
+                    frameAction.setIconImage(icon);
+
+                    ln.setBounds(100, -50, 300, 300);
+
+                    frameAction.setTitle("About Section");
+                    frameAction.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    frameAction.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+
+                    frameAction.setLocationRelativeTo(null);
+
+                    frameAction.add(ln);
+                    frameAction.setLayout(null);
+                    frameAction.setVisible(true);
+
+                    about.add(frameAction);
+                } catch (Exception exp) {
+                    System.out.println(exp);
                 }
 
             }
         });
 
-
         // adding  menubar in main frame
         f.setJMenuBar(mb);
-      // for adding scroll bar when text reaches the max size of window
-        JScrollPane jp=new JScrollPane(t);
+        // for adding scroll bar when text reaches the max size of window
+        JScrollPane jp = new JScrollPane(t);
         f.add(jp);
         // for making horizontal bar hide
         jp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
@@ -136,10 +124,99 @@ public class Notepad {
         jp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         t.setLineWrap(true);
         t.setWrapStyleWord(true);
-  
+
+        newfile.addActionListener(this);
+        openfile.addActionListener(this);
+        savefile.addActionListener(this);
+        printfile.addActionListener(this);
+        exitfile.addActionListener(this);
+        cut.addActionListener(this);
+        copy.addActionListener(this);
+        paste.addActionListener(this);
+        selectall.addActionListener(this);
+        about.addActionListener(this);
+
         // for making frame visible
         f.setVisible(true);
+        
+    }
+    
+
+   
+    
+    
+    
+    
+    
+    
+       
+    public static void main(String[] args) {
+        
+        
+        // default constructor
+        Notepad n=new Notepad();
 
     }
+
+     public  void actionPerformed(ActionEvent e) {
+          if(e.getActionCommand().equalsIgnoreCase("New")){
+             
+             t.setText(null);
+             
+          }
+//          else if(e.getActionCommand().equalsIgnoreCase("Open")){
+//          
+//          }
+//          
+//           else if(e.getActionCommand().equalsIgnoreCase("Save")){
+//          
+//          }
+//           else if(e.getActionCommand().equalsIgnoreCase("Print")){
+//          
+//          }
+//           else if(e.getActionCommand().equalsIgnoreCase("Exit")){
+//          
+//          }
+//           else if(e.getActionCommand().equalsIgnoreCase("Cut")){
+//          
+//          }
+//           else if(e.getActionCommand().equalsIgnoreCase("Copy")){
+//          
+//          }
+//           else if(e.getActionCommand().equalsIgnoreCase("Paste")){
+//          
+//          }
+//           else if(e.getActionCommand().equalsIgnoreCase("Select All")){
+//          
+//          }
+//          
+//           else if(e.getActionCommand().equalsIgnoreCase("About")){
+//          
+//          } 
+//          
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
